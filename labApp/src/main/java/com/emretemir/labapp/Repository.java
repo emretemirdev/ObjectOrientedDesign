86179064350c5006d098deaad0d33e8561f06495
Emre Temir <102259373+emretemirdev@users.noreply.github.com>
2024-03-12 21:18:13 +0300

    Delete Project

--- labApp/src/main/java/com/emretemir/labapp/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.emretemir.labapp;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-@org.springframework.stereotype.Repository
-
-public interface Repository extends MongoRepository<User, String>{
-
-}
