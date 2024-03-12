213dfe0c096d4ad3bcd61ea372ff4340ffc6bdc9
Emre Temir <102259373+emretemirdev@users.noreply.github.com>
2024-03-12 21:17:39 +0300

    Create Repo

---
+++ spring-boot-3-course-chaddarby/src/main/java/com/emretemir/springboot/springboot3coursechaddarby/SpringBoot3CourseChaddarbyApplication.java
@@ -0,0 +1,14 @@
+package com.emretemir.springboot.springboot3coursechaddarby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBoot3CourseChaddarbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3CourseChaddarbyApplication.class, args);
+	}
+}
---
+++ spring-boot-3-course-chaddarby/src/test/java/com/emretemir/springboot/springboot3coursechaddarby/SpringBoot3CourseChaddarbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emretemir.springboot.springboot3coursechaddarby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3CourseChaddarbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
